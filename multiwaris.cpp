a47661eaf0da9696e467d0424d5a374091c22dd2
salmaarayyan <salmaarayyan@gmail.com>
2024-05-27 15:00:46 +0700

    membuat class orang dengan akses public

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 class orang {
+public:
+    int umur;
 
+    orang(int pUmur) :
+        umur(pUmur) {
+            cout << "orang dibuar dengan umur " << umur << "\n" << endl;
+        }
 };
