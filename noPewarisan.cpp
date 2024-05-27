974f6ee1eb3aaa5141812959a4e0b66220d729e0
salmaarayyan <salmaarayyan@gmail.com>
2024-05-27 14:56:09 +0700

    membuat program noPewarisan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-class baseClass {
+class baseClass final {
 public:
-    virtual void perkenalan() final {
+    virtual void perkenalan() {
         cout << "Hallo saya Function dari base class";
     }
 };
